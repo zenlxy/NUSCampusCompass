7695b0ceab97bfd990d5cfac0036e2d8fd05e3a1
janelle <jpow1211@gmail.com>
2024-06-02 22:13:28 +0800

    register with navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,68 +1,19 @@
-import classNames from "classnames";
-import { Text, TextInput, Image, View, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Register from './Register';
+import Login from './Login';
+
+
+const Stack = createStackNavigator();
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Logo />
-      <Username />
-      <Password />
-    </View>
-  );
+    return (
+        <NavigationContainer independent={true}>
+            <Stack.Navigator initialRouteName="Register">
+                <Stack.Screen name="Register" component={Register} />
+                <Stack.Screen name="Login" component={Login} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
-
-const Logo = () => {
-  return (
-    <View>
-      <Image
-        source={require('../assets/images/logo.png')}
-        style={{ width: 180, height: 180 }}
-      />
-    </View>
-  )
-}
-
-const Username = () => {
-  return (
-    <View style={styles.username}>
-      <TextInput style={styles.text} placeholder="Username" />
-    </View>
-  )
-}
-
-const Password = () => {
-  return (
-    <View style={styles.password}>
-      <TextInput style={styles.text} placeholder="Password" />
-    </View>
-  )
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  username: {
-    flex: 0,
-    paddingTop: 25,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  password: {
-    flex: 0,
-    paddingTop: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 20,
-    borderWidth: 1,
-    borderColor: 'black',
-    borderRadius: 15,
-    paddingLeft: 15,
-    padding: 2,
-    paddingRight: 60,
-  }
-})
