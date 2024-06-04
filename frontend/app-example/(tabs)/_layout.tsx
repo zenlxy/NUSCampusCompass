7cbd5a70c9c3517051d647a5c21053aa1e7bc45f
janelle <jpow1211@gmail.com>
2024-06-04 17:42:36 +0800

    frontend folder

--- frontend/app-example/(tabs)/_layout.tsx
+++ frontend/app-example/(tabs)/_layout.tsx
@@ -2,8 +2,8 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 
 import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Colors } from '@/frontend/constants/Colors';
+import { useColorScheme } from '@/frontend/hooks/useColorScheme';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
