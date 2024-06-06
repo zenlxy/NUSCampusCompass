ad5a539bdb79f503487c978f6f8c8fbbe6132ea8
janelle <jpow1211@gmail.com>
2024-06-06 15:01:06 +0800

    labels for login

--- frontend/app/Login/components/password.tsx
+++ frontend/app/Login/components/password.tsx
@@ -1,30 +1,36 @@
-import { View, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 const Password = () => {
-    return (
-      <View style={styles.password}>
-        <TextInput style={styles.text} placeholder="Password" />
-      </View>
-    )
-  }
+  return (
+    <View style={styles.password}>
+      <Text style={styles.label}>Password</Text>
+      <TextInput style={styles.text} placeholder="Password" />
+    </View>
+  )
+}
 
-  const styles = StyleSheet.create({
-    password: {
-      flex: 0,
-      paddingTop: 5,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    text: {
-      fontSize: 20,
-      width: 200,
-      borderWidth: 1,
-      borderColor: 'black',
-      borderRadius: 15,
-      paddingLeft: 15,
-      padding: 2,
-      paddingRight: 60,
-    }
-  })
-  
+const styles = StyleSheet.create({
+  label: {
+    alignSelf: 'flex-start',
+    marginBottom: 2,
+    marginLeft: 5,
+  },
+  password: {
+    flex: 0,
+    paddingTop: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    width: 200,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 15,
+    paddingLeft: 15,
+    padding: 2,
+    paddingRight: 60,
+  }
+})
+
 export default Password;
