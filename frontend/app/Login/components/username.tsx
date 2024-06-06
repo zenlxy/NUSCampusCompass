b638a2f871238f442189a9a8fb4323d71f9d6ea4
janelle <jpow1211@gmail.com>
2024-06-06 15:01:06 +0800

    labels for login

--- frontend/app/Login/components/username.tsx
+++ frontend/app/Login/components/username.tsx
@@ -1,30 +1,36 @@
-import { View, TextInput, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 const Username = () => {
-    return (
-      <View style={styles.username}>
-        <TextInput style={styles.text} placeholder="Username" />
-      </View>
-    )
-  }
+  return (
+    <View style={styles.username}>
+      <Text style={styles.label}>Username</Text>
+      <TextInput style={styles.text} placeholder="Username" />
+    </View>
+  )
+}
 
-  const styles = StyleSheet.create({
-    username: {
-      flex: 0,
-      paddingTop: 25,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    text: {
-        fontSize: 20,
-        width: 200,
-        borderWidth: 1,
-        borderColor: 'black',
-        borderRadius: 15,
-        paddingLeft: 15,
-        padding: 2,
-        paddingRight: 60,
-      }
+const styles = StyleSheet.create({
+  label: {
+    alignSelf: 'flex-start',
+    marginBottom: 2,
+    marginLeft: 5,
+  },
+  username: {
+    flex: 0,
+    paddingTop: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    width: 200,
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 15,
+    paddingLeft: 15,
+    padding: 2,
+    paddingRight: 60,
+  }
 });
 
 export default Username;
