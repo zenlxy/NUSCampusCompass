468235e6ed701b2a200c1d6143252e997ca7e036
janelle <jpow1211@gmail.com>
2024-06-11 22:56:38 +0800

    login backend

--- frontend/app/Register/index.tsx
+++ frontend/app/Register/index.tsx
@@ -2,19 +2,53 @@ import { Text, TextInput, Image, View, Button, TouchableOpacity, StyleSheet } fr
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import Logo from './components/logo';
-import Email from './components/email';
-import Password from './components/password';
-import ConfirmPassword from './components/confirmpassword';
 import RegisterButton from "./components/registerbutton";
+import React, { useState } from 'react';
+import { useAuth } from '../contexts/AuthContext';
 
 const Register = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const { signup } = useAuth();
+    const handleSignUp = async () => {
+        if (password != confirmPassword) {
+            alert('Passwords do not match!');
+            return;
+        }
+        try {
+            await signup(email, password);
+            alert('Registered successfully!')
+            console.log('Registered successfully');
+        } catch (error) {
+            alert((error as Error).message);
+        }
+    }
     return (
         <View style={styles.container}>
             <Logo />
-            <Email />
-            <Password />
-            <ConfirmPassword />
-            <RegisterButton />
+            <View style={styles.textContainer}>
+                <Text style={styles.label}>Email</Text>
+                <TextInput style={styles.text}
+                    placeholder="Email"
+                    value={email}
+                    onChangeText={setEmail} />
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.label}>Password</Text>
+                <TextInput style={styles.text}
+                    placeholder="Password"
+                    value={password}
+                    onChangeText={setPassword} />
+            </View>
+            <View style={styles.textContainer}>
+                <Text style={styles.label}>Confirm Password</Text>
+                <TextInput style={styles.text}
+                    placeholder="Confirm Password"
+                    value={confirmPassword}
+                    onChangeText={setConfirmPassword} />
+            </View>
+            <Button title="Register" onPress={handleSignUp} />
         </View>
     )
 }
@@ -24,6 +58,27 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    textContainer: {
+        flex: 0,
+        paddingTop: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    label: {
+        alignSelf: 'flex-start',
+        marginBottom: 2,
+        marginLeft: 5,
+    },
+    text: {
+        fontSize: 20,
+        width: 240,
+        borderWidth: 1,
+        borderColor: 'black',
+        borderRadius: 15,
+        paddingLeft: 15,
+        padding: 2,
+        paddingRight: 60,
     }
 })
 
