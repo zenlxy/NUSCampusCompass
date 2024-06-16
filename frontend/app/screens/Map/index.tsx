a32d07432be0368fd8e21e97cc5061d8fc306607
Zenda <e1157401@u.nus.edu>
2024-06-16 10:04:57 +0800

    imported map

--- frontend/app/screens/Map/index.tsx
+++ frontend/app/screens/Map/index.tsx
@@ -1,11 +1,23 @@
-import { View, Text } from 'react-native';
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
 
 const Map = () => {
     return (
-        <View>
-            <Text>Map</Text>
+        <View style={styles.container}>
+            <MapView style={styles.map} />
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+});
 
 export default Map;
