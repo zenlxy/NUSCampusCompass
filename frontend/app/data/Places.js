57e602c7e15c88fbe8f865f76805934265cbea43
janelle <jpow1211@gmail.com>
2024-06-19 13:16:10 +0800

    mock data for places

---
+++ frontend/app/data/Places.js
@@ -0,0 +1,31 @@
+const places = [
+    {
+        placeId: 1,
+        name: "Lee Kong Chian Natural History Museum",
+        description: "The Lee Kong Chian Natural History Museum in Singapore showcases the incredible biodiversity of Southeast Asia, including stunning dinosaur skeletons and rare creatures like the giant squid. As part of the National University of Singapore, it serves as a dynamic hub for scientific research and education. The museum offers engaging, interactive exhibits and programs that inspire a deeper appreciation of the natural world. With its eco-friendly design and focus on conservation, it is a key destination for both learning and exploration.",
+        history: "The Lee Kong Chian Natural History Museum is the oldest natural history museum in Southeast Asia, tracing its roots back to the founding of the Raffles Museum by Sir Thomas Stamford Raffles in 1823. Officially recognized as a separate entity in 1878, the museum’s collection has grown through various incarnations, including the Raffles Museum of Biodiversity Research. Established as an independent academic unit in 2014, the museum preserves Singapore's natural heritage and houses extensive historical collections.",
+        funFacts: "1) The museum houses more than 560,000 specimens, including animals, plants, and fossils. 2) The museum features three massive sauropod dinosaur skeletons named \"Prince,\" \"Apollonia,\" and \"Twinky.\" These 80% complete skeletons, found in Wyoming, USA, are among the most significant dinosaur finds and are displayed prominently in the museum.",
+        address: "",
+        coordinates: "",
+    },
+    {
+        placeId: 2,
+        name: "University Cultural Centre",
+        description: "As a high-grade, flexible performance venue nestled in the grounds of a premier university, the University Cultural Centre is a consistently sought-after venue. It operates year-round, hosting University events, arts programmes and external bookings.",
+        history: "The University Cultural Centre (UCC) was established in Singapore in 1986 as a venue dedicated to promoting arts and culture within the National University of Singapore (NUS) campus. Initially conceived to host performances and events by student groups, it quickly expanded its role to become a hub for local and international arts programming. Over the years, UCC has grown to encompass a wide range of artistic disciplines, fostering creativity and cultural exchange through its diverse offerings of concerts, exhibitions, and theatrical productions.",
+        funFacts: "1) Architectural Design: The University Cultural Centre's (UCC) distinctive architecture, designed by Kenzo Tange, incorporates elements that symbolize openness and creativity, reflecting its role as a cultural hub. 2) Celebrity Performances: UCC has hosted performances by renowned artists and groups such as Yo-Yo Ma, the Singapore Symphony Orchestra, and international dance troupes, contributing to its reputation as a premier cultural venue in Singapore. 3) Cultural Diversity: Beyond traditional arts, UCC regularly showcases a variety of cultural performances including traditional music, dance, and theater from different parts of the world, promoting cultural diversity and understanding among its audiences.",
+        address: "",
+        coordinates: "",
+    },
+    {
+        placeId: 3,
+        name: "NUS Museum",
+        description: "As a university museum, NUS Museum’s mission is to actively facilitate intellectual and cultural life within and beyond the University. With a distinct focus on Asia, the Museum contributes to and facilitates the production, reception, and preservation of knowledge through collections development and curatorial practice. The Museum is a cultural hub, developing partnerships within NUS, the culture and heritage industry, and the global knowledge community.",
+        history: "The NUS Museum, originating as the University Art Museum in 1955 at the University of Malaya, predates Singapore's independence. It has evolved through various campus locations to establish a permanent home at the University Cultural Centre in 2000. Today, it houses over 8,000 artifacts and artworks, including significant collections like South & Southeast Asian art and the Straits Chinese Collection at the NUS Baba House, reflecting Singapore's cultural and historical evolution.",
+        funFacts: "1) Virtual Exhibitions: The NUS Museum has developed virtual exhibitions and digital resources that allow global audiences to access its collections and learn about Southeast Asian art and culture remotely. 2) Artist-in-Residence Program: The museum hosts an Artist-in-Residence program, inviting contemporary artists to create and exhibit new works that engage with its collections and themes, fostering creative dialogue between traditional and contemporary art practices.",
+        address: "",
+        coordinates: "",
+    }
+];
+
+export default places;
