af9510dcd4e440505024e15f5581f05aff1c36e9
janelle <jpow1211@gmail.com>
2024-06-21 15:13:31 +0800

    firestore save button

--- frontend/app/screens/Itinerary/screens/build/components/addDates.tsx
+++ frontend/app/screens/Itinerary/screens/build/components/addDates.tsx
@@ -4,6 +4,11 @@ import { StackNavigationProp } from '@react-navigation/stack';
 import React, { useState, ReactNode } from 'react';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import places from '../../../../../data/Places';
+import { addDoc } from 'firebase/firestore';
+import { itineraryRef } from '@/config/firebase';
+import { useAuth } from '../../../../../contexts/AuthContext';
+import Icon from '@expo/vector-icons/FontAwesome6';
+
 type Place = {
     placeId: number;
     name: string;
@@ -21,6 +26,24 @@ const AddDatesButton = () => {
     const [userInput, setUserInput] = useState("");
     const [toAdd, setToAdd] = useState<Place>(places[1]);
     const [itinerary, setItinerary] = useState<Place[]>([]);
+    const { user } = useAuth();
+    const [isEditing, setIsEditing] = useState(false);
+    const [text, setText] = useState('Rename with Title');
+    const [inputValue, setInputValue] = useState(text);
+    const handleBlur = () => {
+        setIsEditing(false);
+        setText(inputValue);
+    };
+    const handleSave = async () => {
+        let doc = await addDoc(itineraryRef, {
+            itinerary,
+            startDate,
+            userId: user.uid,
+        });
+        if (doc && doc.id) {
+            navigation.navigate("Main");
+        }
+    };
     const itineraryList: ListRenderItem<Place> = ({ item, index }) => {
         return (
             <View style={styles.itineraryItem}>
@@ -65,6 +88,32 @@ const AddDatesButton = () => {
     };
     return (
         <View style={styles.bigContainer}>
+            <View style={styles.headercontainer}>
+                <View style={styles.backcontainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate("Main")}>
+                        <Icon name="arrow-left" size={20} color="#047bff" />
+                    </ TouchableOpacity>
+                </View>
+                <View style={styles.titleContainer}>
+                    {isEditing ? (
+                        <TextInput
+                            style={styles.titleInput}
+                            value={inputValue}
+                            onChangeText={setInputValue}
+                            onBlur={handleBlur}
+                            onSubmitEditing={handleBlur}
+                            autoFocus
+                        />
+                    ) : (
+                        <View style={styles.displayContainer}>
+                            <Text style={styles.titleText}>{text}</Text>
+                            <TouchableOpacity onPress={() => setIsEditing(true)} style={styles.iconContainer}>
+                                <Icon name="pencil" size={20} color="black" />
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </View>
+            </View>
             <View style={styles.container}>
                 <Text>Select Date:</Text>
                 <DateTimePicker
@@ -99,19 +148,26 @@ const AddDatesButton = () => {
                 </View>
             </View>
             <FlatList data={itinerary} renderItem={itineraryList} />
+            <View style={styles.saveContainer}>
+                <TouchableOpacity onPress={handleSave} >
+                    <View style={styles.saveButton}>
+                        <Text style={styles.saveText}>Save</Text>
+                        <Icon name="arrow-down" size={20} color="white" />
+                    </View>
+                </ TouchableOpacity>
+            </View >
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     bigContainer: {
-        marginTop: 10,
         justifyContent: 'center',
     },
     container: {
         width: 'auto',
         alignItems: 'center',
-        justifyContent: 'center',
+        paddingLeft: 20,
         flexDirection: 'row',
     },
     button: {
@@ -184,6 +240,54 @@ const styles = StyleSheet.create({
     },
     basic: {
         marginLeft: 20,
+    },
+    saveContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 30,
+    },
+    saveButton: {
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: 80,
+        backgroundColor: '#047bff',
+        height: 30,
+        borderRadius: 15,
+        paddingLeft: 10,
+        paddingRight: 10,
+    },
+    saveText: {
+        color: 'white',
+        fontSize: 17,
+        fontFamily: 'Arial',
+    },
+    titleContainer: {
+        paddingLeft: 20,
+        paddingTop: 10,
+    },
+    displayContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    titleText: {
+        fontSize: 18,
+    },
+    titleInput: {
+        fontSize: 18,
+        borderBottomWidth: 1,
+        borderBottomColor: '#90B8B8',
+    },
+    iconContainer: {
+        paddingLeft: 10,
+    },
+    headercontainer: {
+        flexDirection: "row",
+    },
+    backcontainer: {
+        paddingLeft: 10,
+        paddingTop: 10,
     }
 })
 
--- frontend/app/screens/Itinerary/screens/build/index.tsx
+++ frontend/app/screens/Itinerary/screens/build/index.tsx
@@ -1,18 +1,10 @@
 import { View, StyleSheet, ScrollView } from 'react-native';
-import Title from './components/title';
 import AddDatesButton from './components/addDates';
-import SubmitButton from './components/submitButton';
-import BackButton from './components/backButton';
 
 const Build = () => {
     return (
         <ScrollView style={styles.bigcontainer}>
-            <View style={styles.container}>
-                <BackButton />
-                <Title />
-            </View>
             <AddDatesButton />
-            <SubmitButton />
         </ScrollView>
     );
 };
@@ -20,9 +12,6 @@ const Build = () => {
 const styles = StyleSheet.create({
     bigcontainer: {
         marginBottom: 93,
-    },
-    container: {
-        flexDirection: "row",
     }
 })
 
