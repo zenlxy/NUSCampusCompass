52d4dc0148731db9b306c06c5748d4d3c52386c4
janelle <jpow1211@gmail.com>
2024-06-22 09:55:27 +0800

    fetch data for existing itineraries

--- frontend/app/screens/Itinerary/screens/existing/index.tsx
+++ frontend/app/screens/Itinerary/screens/existing/index.tsx
@@ -1,11 +1,59 @@
-import { View, Text } from 'react-native';
+import { useAuth } from '@/app/contexts/AuthContext';
+import { itineraryRef } from '@/config/firebase';
+import { getDocs, query, where } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { View, FlatList, ListRenderItem, StyleSheet, Text, Button } from 'react-native';
 
 const Existing = () => {
+    const [trips, setTrips] = useState([]);
+    const { user } = useAuth();
+    const fetchTrips = async () => {
+        const q = query(itineraryRef, where("userId", "==", user.uid));
+        const querySnapshot = await getDocs(q);
+        let data: any = [];
+        querySnapshot.forEach(doc => {
+            data.push({ ...doc.data(), id: doc.id })
+        });
+        setTrips(data);
+    };
+    useEffect(() => {
+        fetchTrips();
+    }, [])
+    const itineraryList: ListRenderItem<any> = ({ item, index }) => {
+        return (
+            <View >
+                <Text >{item.text}</Text>
+            </View>
+        );
+        return null;
+    };
     return (
         <View>
-            <Text>Existing Itineraries</Text>
+            <FlatList data={trips} renderItem={itineraryList}></FlatList>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        width: 'auto',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 70,
+    },
+    button: {
+        backgroundColor: '#90B8B8',
+        width: '70%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 50,
+        borderRadius: 15,
+    },
+    text: {
+        fontSize: 17,
+        color: 'black',
+        fontFamily: 'Rubik'
+    },
+})
 
 export default Existing;
