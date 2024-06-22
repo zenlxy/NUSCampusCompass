3ad45dc18f2638a45cc52eb733f96b2bd3e61e75
janelle <jpow1211@gmail.com>
2024-06-22 20:06:59 +0800

    ui for existing itineraries + unique title

--- frontend/app/screens/Itinerary/screens/existing/index.tsx
+++ frontend/app/screens/Itinerary/screens/existing/index.tsx
@@ -2,7 +2,8 @@ import { useAuth } from '@/app/contexts/AuthContext';
 import { itineraryRef } from '@/config/firebase';
 import { getDocs, query, where } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
-import { View, FlatList, ListRenderItem, StyleSheet, Text, Button } from 'react-native';
+import { View, FlatList, ListRenderItem, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { green } from 'react-native-reanimated/lib/typescript/reanimated2/Colors';
 
 const Existing = () => {
     const [trips, setTrips] = useState([]);
@@ -16,13 +17,31 @@ const Existing = () => {
         });
         setTrips(data);
     };
+
     useEffect(() => {
         fetchTrips();
-    }, [])
+    }, []);
+
+    const handleStart = () => {
+        console.log('Start Trip');
+    };
+
+    const handleEdit = () => {
+        console.log('Edit Trip');
+    };
+
     const itineraryList: ListRenderItem<any> = ({ item, index }) => {
         return (
-            <View >
-                <Text >{item.text}</Text>
+            <View style={styles.container}>
+                <Text style={styles.text}>{item.text}</Text>
+                <View>
+                    <TouchableOpacity style={styles.button} onPress={handleStart}>
+                        <Text>Start</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.button, { marginBottom: 10 }]} onPress={handleEdit}>
+                        <Text>Edit</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         );
         return null;
@@ -36,23 +55,30 @@ const Existing = () => {
 
 const styles = StyleSheet.create({
     container: {
-        width: 'auto',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'center',
-        marginTop: 70,
+        flexDirection: 'row',
+        backgroundColor: '#90B8B8',
+        marginHorizontal: 20,
+        marginTop: 20,
+        borderRadius: 20,
     },
     button: {
-        backgroundColor: '#90B8B8',
-        width: '70%',
+        backgroundColor: '#ffe8b8',
         justifyContent: 'center',
         alignItems: 'center',
-        height: 50,
-        borderRadius: 15,
+        borderRadius: 10,
+        height: 30,
+        width: 50,
+        marginRight: 10,
+        marginTop: 10,
     },
     text: {
-        fontSize: 17,
+        fontSize: 20,
         color: 'black',
-        fontFamily: 'Rubik'
+        fontFamily: 'Arial Bold',
+        marginLeft: 20,
+        width: 240,
     },
 })
 
