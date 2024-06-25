61e5c323ac7958e6058391ef1dd7355b68c2a18d
Zenda <e1157401@u.nus.edu>
2024-06-25 15:47:04 +0800

    maps3

--- frontend/app/screens/Map/components/appmapview.tsx
+++ frontend/app/screens/Map/components/appmapview.tsx
@@ -1,22 +1,33 @@
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import * as Location from 'expo-location';
 import { UserLocationContext } from '../../../contexts/UserLocationContext';
 import { useContext } from 'react';
 import { Region } from 'react-native-maps';
+import Icon from '@expo/vector-icons/FontAwesome6';
 
 const AppMapView = () => {
     const { location, setLocation } = useContext<any>(UserLocationContext);
-    return location?.latitude && (
+    return location?.coords?.latitude && (
         <View>
             <MapView
                 style={styles.map} 
                 initialRegion={{
-                    latitude: location?.latitude,
-                    longitude: location?.longitude,
+                    latitude: location?.coords?.latitude || 1.3521,
+                    longitude: location?.coords?.longitude || 103.8198,
                     latitudeDelta: 0.0422,
                     longitudeDelta: 0.0421,
-                }} />
+                }} 
+            >
+                <Marker
+                    coordinate={{
+                        latitude: location?.coords?.latitude,
+                        longitude: location?.coords?.longitude
+                    }}
+                > 
+                    <Icon name="location-dot" size={30} color="#047bff" />
+                </Marker>
+            </MapView>
         </View>
     )
 }
