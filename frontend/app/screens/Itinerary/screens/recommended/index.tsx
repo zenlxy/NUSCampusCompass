bae69a1204d69ba3b5ac7bd8fceac5410747e376
janelle <jpow1211@gmail.com>
2024-06-25 12:53:50 +0800

    recommended + delete for existing

--- frontend/app/screens/Itinerary/screens/recommended/index.tsx
+++ frontend/app/screens/Itinerary/screens/recommended/index.tsx
@@ -1,11 +1,60 @@
-import { View, Text } from 'react-native';
+import { View, Text, ListRenderItem, FlatList, TouchableOpacity, StyleSheet } from 'react-native';
+import recommended from '../../../../data/Recommended';
 
 const Recommended = () => {
+    const handleStart = () => {
+        console.log('Start Trip');
+    };
+    const itineraryList: ListRenderItem<any> = ({ item, index }) => {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>{item.text}</Text>
+                <View>
+                    <TouchableOpacity style={styles.button} onPress={handleStart}>
+                        <Text>Start</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+        return null;
+    };
     return (
         <View>
-            <Text>Recommended Tours</Text>
+            <FlatList data={recommended}
+                renderItem={itineraryList}
+            ></FlatList>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flexDirection: 'row',
+        backgroundColor: '#90B8B8',
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 20,
+        borderRadius: 20,
+    },
+    button: {
+        backgroundColor: '#ffe8b8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+        height: 30,
+        width: 50,
+        marginRight: 10,
+        marginVertical: 10,
+    },
+    text: {
+        fontSize: 20,
+        color: 'black',
+        fontFamily: 'Arial Bold',
+        marginLeft: 20,
+        width: 220,
+    },
+})
 
 export default Recommended;
