b0dc323f54f84503b5141ecdc5891344742434cf
Zenda <e1157401@u.nus.edu>
2024-06-25 21:27:40 +0800

    maps5

--- frontend/app/screens/Map/index.tsx
+++ frontend/app/screens/Map/index.tsx
@@ -3,14 +3,25 @@ import { TextInput, Image, TouchableOpacity, StyleSheet, View } from 'react-nati
 import * as Location from 'expo-location';
 import { UserLocationContext } from '../../contexts/UserLocationContext';
 import AppMapView from './components/appmapview';
+import { Coordinates, Place } from '@/app/types/types';
+import places from '@/app/data/Places';
+import MapView, { Marker } from 'react-native-maps';
+import Icon from '@expo/vector-icons/FontAwesome6';
 
-const Map = () => {
+const locationsOfInterest = places.map((place: Place) => ({
+  title: place.name,
+  location: place.coordinates,
+  description: place.description,
+}));
+
+
+function Map() {
   const [location, setLocation] = useState<Location.LocationObject | null>(null);
   const [errorMsg, setErrorMsg] = useState<string | null>(null);
 
   useEffect(() => {
     (async () => {
-      
+
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
@@ -30,16 +41,49 @@ const Map = () => {
     text = JSON.stringify(location);
   }
 
+  const showLocationsOfInterest = () => {
+    return locationsOfInterest.map((item, index) => {
+      return (
+        <Marker
+          key={index}
+          coordinate={item.location}
+          title={item.title}
+          description={item.description}
+        >
+        <Icon name="location-dot" size={25} color="red" />
+        </Marker>
+      )
+    });
+  }
+  
   return (
-    <UserLocationContext.Provider value={{location, setLocation} as any}>
-    <View style={styles.container}>
-      <AppMapView />
-      <Search />
-      <Filter />
-    </View>
-    </UserLocationContext.Provider> 
+    <UserLocationContext.Provider value={{ location, setLocation } as any}>
+      <View style={styles.container}>
+      <MapView
+                style={styles.map} 
+                initialRegion={{
+                    latitude: location?.coords?.latitude || 1.3020,
+                    longitude: location?.coords?.longitude || 103.7730,
+                    latitudeDelta: 0.005,
+                    longitudeDelta: 0.005,
+                }} 
+            >
+                <Marker
+                  coordinate={{
+                    latitude: location?.coords?.latitude || 0,
+                    longitude: location?.coords?.longitude || 0
+                  }}
+                > 
+                  <Icon name="location-dot" size={25} color="#047bff" />
+                </Marker>
+                {showLocationsOfInterest()}
+            </MapView>
+        <Search />
+        <Filter />
+      </View>
+    </UserLocationContext.Provider>
   );
-};
+}
 
 const Search = () => {
   return (
@@ -76,6 +120,10 @@ const Filter = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
   searchContainer: {
     position: 'absolute',
